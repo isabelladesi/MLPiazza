3903be42be043b306a02bbda9dad6b3457e2bf6d
idesousa <idesousa@umich.edu>
2023-11-27 09:02:22 -0500

    newest updates

--- BinarySearchTree.hpp
+++ BinarySearchTree.hpp
@@ -403,8 +403,12 @@ private:
   // EFFECTS: Frees the memory for all nodes used in the tree rooted at 'node'.
   // NOTE:    This function must be tree recursive.
   static void destroy_nodes_impl(Node *node) {
+    // if (!node){ //node empty
+
+    // }
     //start deleting at bottom of path and trickle back to delete all of them
     assert(false);
+    
   }
 
   // EFFECTS : Searches the tree rooted at 'node' for an element equivalent
