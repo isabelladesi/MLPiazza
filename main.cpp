a086960614c93c6a470521b63bc2b2e37a616388
idesousa <idesousa@umich.edu>
2023-11-28 15:28:39 -0500

    test

--- main.cpp
+++ main.cpp
@@ -14,7 +14,46 @@ class Classifier{
     word(trainStream);
     labelWord(trainStream);
   }
-  void test(csvstream & testStream);
+  // void test(csvstream & testStream); { //prediction (math) (parameters: a label, string ofwords in new post)
+  //   set<string> uniqueWords = unique_words(testStream); //gives unique words string
+
+  //   //double prediction = 0;
+
+  //   //log prior
+  //   // double logPrior;
+  //   // int numCTrainP = ...    //number of training posts with label C
+  //   // int numTrainPosts = ...        // number of training posts
+  //   //logPrior = log(numCTrainP/numTrainPosts);
+  //   //prediction += logPrior;
+
+  //   for(int i=0; i<uniqueWords.size(); ++i){// each unique word
+  //     //int numCTrainPW = // =function that says how many times word appears in the traininposts with label C ...[i]; //number of training posts with label C that contain W
+  //     //int numTrainPW = //=function that says how many times word appears in all training posts regardless of label
+      
+  //     //log liklihood
+  //     //double logLikl = 0;
+  //     if(numCTrainPW==0 && numTrainPW!=0){ //special case 1
+  //       logLikl = log(numTrainPW/numTrainPosts);
+  //     }
+  //     else if(numCTrainPW==0 && numTrainPW==0){
+  //       logLikl = log(1/numTrainPosts);
+
+  //     }
+  //     else{
+  //       logLikl = log(numCTrainPW/numCTrainP);
+  //     }
+
+  //     prediction += logLikl;
+  //     logLikl=0;
+
+  //   }
+  //   return prediction; //will return prediction score for a particular label
+  //   // for each label find its score using equation
+  //   // the classifier will pick the label with the highest score
+  //   // there should be n log likelihood terms. n = number of UNIQUE words in string (post)
+  //   //in divisions: cast the numer and denom as double before 
+
+  // }
 
   private:
   //training steps
